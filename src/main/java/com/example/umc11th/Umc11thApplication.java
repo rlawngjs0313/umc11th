a176c64c4f65aa5fbe0037a19c4c5b100aef6b39
JuHeon <juheon0313@naver.com>
2026-08-25 17:13:09 +0900

    🚀 chore: 프로젝트 세팅

---
+++ src/main/java/com/example/umc11th/Umc11thApplication.java
@@ -0,0 +1,13 @@
+package com.example.umc11th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc11thApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Umc11thApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/umc11th/Umc11thApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umc11th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umc11thApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
